d8fc66e1c263e7fde87b56c1b6b8235f868ecbd6
Mubir Hussain <117667092+Mubir786@users.noreply.github.com>
2025-07-10 08:25:24 +0000

    FruitDetectionSystem config changes

---
+++ fruitdetectionsystem/src/Components/App.js
@@ -0,0 +1,20 @@
+import react from 'react';
+import Header from './Header';
+import AddContact from './AddContact';
+import ContactList from './ContactList';
+import ContactCard from './ContactCard';
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      {/*<ContactList/>
+      <AddContact/>
+      <ContactCard/>*/}
+    </div>
+  );
+}
+
+export default App;
